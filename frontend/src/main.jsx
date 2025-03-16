f8c6fc90ba1fffa301e2839c4e762a18b1f400c9
janiussyafiq <janiussyafiq@wisc.edu>
2025-03-16 15:14:42 -0500

    Fetched suggested users

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,10 +5,18 @@ import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import { QueryClientProvider, QueryClient } from '@tanstack/react-query'
 
+const defaultQueryFn = async ({ queryKey }) => {
+  const { data } = await axios.get(
+    `https://jsonplaceholder.typicode.com${queryKey[0]}`,
+  )
+  return data
+}
+
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
       refetchOnWindowFocus: false,
+      queryFn: defaultQueryFn,
     },
   },
 });
