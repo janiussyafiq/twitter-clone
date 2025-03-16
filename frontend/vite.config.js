79b976ab0928cf0c068a93bf72bc654a2beadfe1
janiussyafiq <janiussyafiq@wisc.edu>
2025-03-16 14:22:30 -0500

    Handled auth and routes protected in client

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -9,6 +9,12 @@ export default defineConfig({
     tailwindcss(),
   ],
   server: {
-    port: 3000
+    port: 3000,
+    proxy: {
+      "/api": {
+        target: "http://localhost:7002",
+        changeOrigin: true,
+      }
+    }
   }
 })
