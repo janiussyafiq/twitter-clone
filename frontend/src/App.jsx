d5439ec31a4beb8e4f5f6e54b0fbfdd5cb6426e7
janiussyafiq <janiussyafiq@wisc.edu>
2025-03-16 23:57:18 -0500

    Updated typo on App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import { Navigate, Route, Routes } from "react-router-dom";
 import HomePage from "./pages/home/HomePage";
-import SignUpPage from "./pages/auth/signup/SIgnUpPage";
+import SignUpPage from "./pages/auth/signup/SignUpPage";
 import LoginPage from "./pages/auth/login/LoginPage";
 import NotificationPage from "./pages/notification/NotificationPage";
 import ProfilePage from "./pages/profile/ProfilePage";
